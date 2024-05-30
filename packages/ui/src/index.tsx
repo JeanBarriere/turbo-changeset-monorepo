52f9cc136e011e58ec03cef216c91b442d67ebd6
Jean Barriere <jean.barriere@shell.com>
2024-05-30 15:54:11 +0200

    feat(uilib): add avatar component

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,9 +1,9 @@
-import { Theme, Button, IconButton } from '@radix-ui/themes';
+import { Theme, Button, Avatar } from '@radix-ui/themes';
 
 import '@radix-ui/themes/styles.css';
 
 export const UI = {
     Theme,
     Button,
-    IconButton,
+    Avatar,
 }
