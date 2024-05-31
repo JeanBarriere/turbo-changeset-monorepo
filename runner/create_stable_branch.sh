1a5a420c43b360b8745ffb2415043bf68de4a8f5
Jean Barriere <jean.barriere@shell.com>
2024-05-31 18:19:27 +0200

    feat(runner): check in pre.json if tag is not found

--- runner/create_stable_branch.sh
+++ runner/create_stable_branch.sh
@@ -7,17 +7,44 @@ PKG_VERSION=$(cat package.json | jq -r '.version') # extract the package version
 PKG_TAG=$(echo "$PKG_NAME@$PKG_VERSION") # generate the git tag of that package last stable release
 PKG_VERSION_MAJOR=$(echo $PKG_VERSION | cut -d '.' -f 1) # extract the current major version
 
-# get the first breaking change since that version
-# reverse logs from HEAD to the last tag (git log)
-# find the first breaking change commit sha (grep + cut)
-COMMIT_SHA=$(git log $PKG_TAG..HEAD --oneline --reverse | grep -m 1 ")\!" | cut -d ' ' -f 1)
+# check if the tag exists
+if git rev-parse --verify $PKG_TAG > /dev/null 2>&1; then
+  # get the first breaking change since that version
+  # reverse logs from HEAD to the last tag (git log)
+  # find the first breaking change commit sha (grep + cut)
+  COMMIT_SHA=$(git log $PKG_TAG..HEAD --oneline --reverse | grep -m 1 ")\!" | cut -d ' ' -f 1)
 
-if [ -z "$COMMIT_SHA" ]; then
-  echo "No breaking change found since $PKG_TAG"
-  exit 0
+  if [ -z "$COMMIT_SHA" ]; then
+    echo "No breaking change found since $PKG_TAG"
+    exit 0
+  else
+    echo "Breaking change found in commit $COMMIT_SHA since $PKG_TAG"
+  fi
 else
-  echo "Breaking change found in commit $COMMIT_SHA since $PKG_TAG"
+  echo "Tag $PKG_TAG does not exist"
+  # get the tag from the last stable release in pre.json
+  PKG_VERSION=$(cat ../../.changeset/pre.json | jq --arg PKG_NAME "$PKG_NAME" -r '.initialVersions | to_entries | map(select(.key == $PKG_NAME) | .value)[]')
+  PKG_TAG=$(echo "$PKG_NAME@$PKG_VERSION") # generate the git tag of that package last stable release
+  PKG_VERSION_MAJOR=$(echo $PKG_VERSION | cut -d '.' -f 1) # extract the current major version
+
+  if git rev-parse --verify $PKG_TAG > /dev/null 2>&1; then
+    echo "Tag $PKG_TAG found"
+  else
+    echo "Tag $PKG_TAG not found"
+    exit 0
+  fi
+
+  COMMIT_SHA=$(git log $PKG_TAG..HEAD --oneline --reverse | grep -m 1 ")\!" | cut -d ' ' -f 1)
+
+  if [ -z "$COMMIT_SHA" ]; then
+    echo "No breaking change found since $PKG_TAG"
+    exit 0
+  else
+    echo "Breaking change found in commit $COMMIT_SHA since $PKG_TAG"
+  fi
 fi
+
+
 
 # check if branch exists
 if git rev-parse --verify release/${CURRENT_DIR}/${PKG_VERSION_MAJOR} > /dev/null 2>&1; then
