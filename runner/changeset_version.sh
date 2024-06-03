890190793e084a9de1e946284863c8d69da9b32b
Jean Barriere <jean.barriere@shell.com>
2024-06-03 14:51:24 +0200

    chore(monorepo): get changeset scripts from main

---
+++ runner/changeset_version.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# Exit any previous pre mode
+pnpm changeset pre exit
+
+# check if we are looking for unstable release
+if [ "$1" = "unstable" ]; then
+  echo "Unstable release mode"
+
+  # check if there is any major changeset in progress
+  if grep -qe "major" -R .changeset/*.md; then
+    echo "Major changeset detected, entering pre next-major mode"
+    pnpm changeset pre enter next-major
+  else
+    echo "No major changeset detected, entering pre beta mode"
+    pnpm changeset pre enter beta
+  fi
+else
+  echo "Stable release mode"
+  PACKAGE_DIR=$1;
+  # get package name
+  PACKAGE_NAME=$(cd $PACKAGE_DIR && pnpm pkg get name | jq -r '.')
+  # all available packages
+  PKG_NAMES="[\"@pkges/web\", \"@pkges/docs\", \"@pkges/libui\", \"@pkges/utils\", \"@pkges/runner\"]"
+  # remove $PACKAGE_NAME from JSONPKGNAMES
+  NEWCONFIG=$(echo $PKG_NAMES | jq --arg PACKAGE_NAME "$PACKAGE_NAME" -r 'map(select(. != $PACKAGE_NAME))')
+  # new file
+  TMP_FILE=$(mktemp)
+  # create new config
+  jq --argjson CONFIG "$NEWCONFIG" '.ignore=$CONFIG' .changeset/config.json > $TMP_FILE
+  # save old config
+  mv .changeset/config.json .changeset/config.json.bak
+  # replace old config with new config
+  mv $TMP_FILE .changeset/config.json
+fi
+
+# update versions
+pnpm changeset version
+
+# if we are in stable mode, restore the old config
+if [ "$1" != "unstable" ]; then
+  mv .changeset/config.json.bak .changeset/config.json
+fi
