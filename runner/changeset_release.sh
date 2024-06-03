fb4084f196680051115f74c9e0f5e20090de2565
Jean Barriere <jean.barriere@shell.com>
2024-06-03 17:38:35 +0200

    chore(monorepo): rebase main runner scripts

--- runner/changeset_release.sh
+++ runner/changeset_release.sh
@@ -2,7 +2,7 @@
 
 GIT_BRANCH=$(git branch --show-current)
 # get the version from the branch (release/{package}/{version})
-VERSION=$(echo $GIT_BRANCH | cut -d'/' -f 3)
+VERSION=$(echo $GIT_BRANCH | grep -ioe "\d*$")
 
 # build apps & packages
 pnpm turbo build
@@ -12,7 +12,7 @@ if [ "$GIT_BRANCH" = "main" ]; then
   echo "Publishing the main branch"
   pnpm changeset publish
 # if there is a version, run release
-elif [[ "$VERSION" =~ ^[0-9]+$ ]]; then
+elif [ -n "$VERSION" ]; then
   echo "Publishing $GIT_BRANCH branch under the version $VERSION tag"
   # get mode and tag from changeset pre.json
   CHANGESET_MODE=$(cat .changeset/pre.json | jq -r '.mode')
