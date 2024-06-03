fcafde3d2bfc514325c6e84f67795c80a6fe033e
Jean Barriere <jean.barriere@shell.com>
2024-06-03 12:40:59 +0200

    chore(@pkges/ui): add comments

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -2,6 +2,9 @@ import { Theme, Button, Avatar } from '@radix-ui/themes';
 
 import '@radix-ui/themes/styles.css';
 
+/**
+ * We export the Theme and Button components from the Radix UI library.
+ */
 export const UI = {
     Theme,
     Button,
