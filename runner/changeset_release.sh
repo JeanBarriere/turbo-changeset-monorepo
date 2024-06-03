d628e7de2142fb8f01ebad4746e6c24cf503ecaa
Jean Barriere <jean.barriere@shell.com>
2024-06-03 17:56:11 +0200

    fix(runner): attempt to fix grep

--- runner/changeset_release.sh
+++ runner/changeset_release.sh
@@ -2,7 +2,7 @@
 
 GIT_BRANCH=$(git branch --show-current)
 # get the version from the branch (release/{package}/{version})
-VERSION=$(echo $GIT_BRANCH | grep -ioe "\d*$")
+VERSION=$(echo $GIT_BRANCH | grep -Eow "[0-9]*$")
 
 # build apps & packages
 pnpm turbo build
